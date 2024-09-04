1d4a8768f81e6a569a995079d87284cff77766d6
alexander1914 <alexander.oliveira99@gmail.com>
2024-09-04 19:29:45 +0100

    Criando view para home da aplicação

--- products-catolog/frontend/src/app/app.component.ts
+++ products-catolog/frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/template/header/header.component";
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, NavComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NavComponent, HomeComponent],
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
--- products-catolog/frontend/src/app/components/template/nav/nav.component.ts
+++ products-catolog/frontend/src/app/components/template/nav/nav.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { MatSidenavModule} from '@angular/material/sidenav'
 import { MatListModule } from '@angular/material/list'
+import { HomeComponent } from "../../../views/home/home.component";
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule],
+  imports: [MatSidenavModule, MatListModule, HomeComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
