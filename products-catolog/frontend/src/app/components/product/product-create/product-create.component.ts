2d27af58e82023e1c89827cb0a5d08b361e8c22f
alexander.oliveira <alexander.oliveira99@gmail.com>
2024-09-16 15:57:57 +0100

    Created news services and new component read

--- products-catolog/frontend/src/app/components/product/product-create/product-create.component.ts
+++ products-catolog/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,25 +1,41 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { FormsModule } from '@angular/forms'
 import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatCardModule } from '@angular/material/card';
+
 import { ProductService } from '../product.service';
+import { Product } from '../product.model';
+
 
 @Component({
   selector: 'app-product-create',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule, MatCardModule],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css'
 })
 export class ProductCreateComponent {
+
+  product: Product = {
+    name: '',
+    price: null
+  }
     
-  constructor(private producService: ProductService, 
+  constructor(private productService: ProductService, 
     private router: Router){ }
 
   ngOnInit(): void{        
   }
   
   createProduct(): void {
-    this.producService.showMessage("Testando a minha service...")
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Produto Criado com Sucessso !")
+      this.router.navigate(['/products'])
+    })    
   }
 
   cancel(): void {
--- products-catolog/frontend/src/app/views/product-crud/product-crud.component.ts
+++ products-catolog/frontend/src/app/views/product-crud/product-crud.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button'
+import { ProductReadComponent } from "../../components/product/product-read/product-read.component";
 
 @Component({
   selector: 'app-product-crud',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, ProductReadComponent],
   templateUrl: './product-crud.component.html',
   styleUrl: './product-crud.component.css'
 })
