df5db69044f717f13d5932f5b1848977216fbaac
alexander.oliveira <alexander.oliveira99@gmail.com>
2024-09-18 18:56:31 +0100

    I did a integration com dbservice on component read

--- products-catolog/frontend/src/app/components/product/product-read/product-read.component.ts
+++ products-catolog/frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, LOCALE_ID } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
+
+import { MatTableModule, MatTable } from '@angular/material/table';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt)
 
 @Component({
   selector: 'app-product-read',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatTable, CommonModule],
+  providers: [{ 
+    provide: LOCALE_ID,
+    useValue: 'pt-PT'
+  }],  
   templateUrl: './product-read.component.html',
   styleUrl: './product-read.component.css'
 })
 export class ProductReadComponent implements OnInit {
 
-  products: Product[] = [];
+  products: Product[] = []
+  displayedColumns = ['id', 'name', 'price', 'action']  
 
   constructor(private productService: ProductService){}
 
